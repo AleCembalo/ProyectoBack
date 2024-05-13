79cc6072e1fcbb1f8a6e2ed1b54cd57b43115f27
AleCembalo <alejandracembalo@hotmail.com>
2024-05-13 12:44:23 -0300

    segundo_commit

--- src/Routes/viewsRouter.js
+++ src/Routes/viewsRouter.js
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import fs from 'fs';
 import path from 'path';
 import config from '../Config/config.js';
-import ProductManager from '../Manager/productManager.js';
+// import ProductManager from '../Manager/productManager.js';
 
 const viewsRouter = Router();
-const productManager = new ProductManager();
+// const productManager = new ProductManager();
 
 const urlProd = path.join(config.DIRNAME, '/Data/products.json');
 const data = await fs.promises.readFile(urlProd, 'utf-8');
@@ -16,31 +16,15 @@ viewsRouter.get('/products', async (req, res) => {
     res.render('home', { products: products });
 });
 
-viewsRouter.get('/', async (req, res) => {
-    try {
-        const products = await productManager.getProducts();
-
-        res.render('home', { title: 'Home', products: products });
-
-    } catch (error) {
-        console.error('Error al obtener los productos:', error);
-        res.status(500).send('Error interno del servidor');
-    }
+viewsRouter.get('/realtimeproducts', async (req, res) => {
+    res.render('realtimeproducts', { products: products });
 });
 
-viewsRouter.post('/realtimeproducts', async (req, res) => {
-    try {
-        const newProduct = req.body;
-        await productManager.addProduct(newProduct);
-
-        const updatedProducts = await productManager.getProducts();
-        io.emit('newProduct', updatedProducts) 
-
-        res.status(200).json({ status: 'success', message: 'Product added successfully' });
-    } catch (error) {
-        res.status(500).json({ status: 'error', message: error.message });
-    }
+viewsRouter.get('/chat', async (req, res) => {
+    res.render('chat', { });
 });
+
+
 
 export default viewsRouter;
 
--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ import config from './Config/config.js';
 import productRouter from './Routes/productsRouter.js';
 import cartsRouter from './Routes/cartsRouter.js';
 import viewsRouter from './Routes/viewsRouter.js';
+// import ProductManager from './Manager/productManager.js';
 
 const app = express();
+// const productManager = new ProductManager();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,50 +26,53 @@ app.use('/static', express.static(`${config.DIRNAME}/public`));
 const httpServer = app.listen ( config.PORT, () => { console.log (`Servidor activo en puerto ${config.PORT}`); });
 
 const socketServer = new Server(httpServer);
-
 app.set('socketServer', socketServer);
 
-socketServer.on('connection', async (socket) => {
-    console.log("Nueva conexión");
+socketServer.on('connection', client => {
+    console.log(`Cliente conectado, id ${client.id} desde ${client.handshake.address}`);
 
-    try {
-        const products = await productManager.getProducts();
-        socketServer.emit('products', products)
-    } catch (error) {
-        socketServer.emit('response', { status: 'error', message: error.message });
-    };
-
-    socket.on('newProduct', async (newProduct) => {
-        try {
-            const newProd = {
-                title: newProduct.title,
-                description: newProduct.description,
-                code: newProduct.code,
-                price: newProduct.price,
-                status: newProduct.status,
-                stock: newProduct.stock,
-                category: newProduct.category,
-                thumbnail: newProduct.thumbnail,
-            }
-
-            await productManager.addProduct(newProd);
-            const updatedList = await productManager.getProducts();
-            socketServer.emit('products', updatedList);
-            socketServer.emit('response', { status: 'success', message: `Product added successfully` })
-        } catch (error) {
-            socketServer.emit('response', { status: 'error', message: error.message });
-        }
+    client.on('newMessage', data => {
+        console.log(`Mensaje recibido desde ${client.id}: ${data}`);
+        client.emit('newMessageConfirmation', 'OK');
     });
+    // try {
+    //     const products = await productManager.getProducts();
+    //     socketServer.emit('products', products)
+    // } catch (error) {
+    //     socketServer.emit('response', { status: 'error', message: error.message });
+    // };
 
-    socket.on('deleteProduct', async (id) => {
-        try {
-            const pId = parseInt(id);
-            await productManager.deleteProduct(pId);
-            const updatedList = await productManager.getProducts();
-            console.log(`Product with id ${pId} successfully deleted`);
-            socketServer.emit('products', updatedList);
-        } catch (error) {
-            socketServer.emit('response', { status: 'error', message: error.message });
-        }
-    })
+    // socket.on('newProduct', async (newProduct) => {
+    //     try {
+    //         const newProd = {
+    //             title: newProduct.title,
+    //             description: newProduct.description,
+    //             code: newProduct.code,
+    //             price: newProduct.price,
+    //             status: newProduct.status,
+    //             stock: newProduct.stock,
+    //             category: newProduct.category,
+    //             thumbnail: newProduct.thumbnail,
+    //         }
+
+    //         await productManager.addProduct(newProd);
+    //         const updatedList = await productManager.getProducts();
+    //         socketServer.emit('products', updatedList);
+    //         socketServer.emit('response', { status: 'success', message: `Product added successfully` })
+    //     } catch (error) {
+    //         socketServer.emit('response', { status: 'error', message: error.message });
+    //     }
+    // });
+
+    // socket.on('deleteProduct', async (id) => {
+    //     try {
+    //         const pId = parseInt(id);
+    //         await productManager.deleteProduct(pId);
+    //         const updatedList = await productManager.getProducts();
+    //         console.log(`Product with id ${pId} successfully deleted`);
+    //         socketServer.emit('products', updatedList);
+    //     } catch (error) {
+    //         socketServer.emit('response', { status: 'error', message: error.message });
+    //     }
+    // })
 });    
